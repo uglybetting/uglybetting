f4b776baa5e6fc7584836173a29948cd2dfd7da1
dobbmeep <dobbmeep@tutanota.com>
2018-05-07 10:19:48 +0100

    second stab at getting files

--- src/batch_files/update_nap_intraday.bat
+++ src/batch_files/update_nap_intraday.bat
@@ -1,3 +1,4 @@
 @echo off
 cmd /c "set PYTHONPATH=%PYTHONPATH%;D:\Python\uglybetting\"
 cmd /c "activate uglybetting & cd /d D:\Python\uglybetting\src\scripts\ & python update_nap_intraday.py"
+pause
